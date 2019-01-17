f0322740cb5b8d45b477f2870d0670d656467c45
hninanya <32991506+hninanya@users.noreply.github.com>
2019-01-16 23:53:20 -0200

    1

--- source/Prueba.cpp
+++ source/Prueba.cpp
@@ -301,7 +301,8 @@ int main() {
 	::std::cout << setw(WIDTH) << RINF << setw(WIDTH) << RSUP << setw(WIDTH) << NRAD << endl;
 	ofstream prueba;
 	prueba.open("C:/Users/Hugo Ninnanya/Documents/GibHub/aninanya/BOORE/BOORE/results/prueba.txt");
-	prueba << "eses"<<endl;
+	prueba << "escgfxdgxdfges"<<endl;
+	cout << "Hola" << endl;
 
 	for (size_t i = 0; i < periodsreq.size(); i++)
 	{
